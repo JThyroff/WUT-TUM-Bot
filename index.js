1d9bd4188e98c6c4afc9a10bcb5c92a701528134
Matthias Helbich <helbich@in.tum.de>
2020-05-19 03:39:35 +0200

    Added comments

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
+// Import the discord.js module
 const Discord = require("discord.js")
+
+// Create an instance of a Discord client
 const client = new Discord.Client()
+
 const { Client, MessageEmbed } = require('discord.js');
+
+// Wait for bot to be ready before reacting to Commands
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
+
+// Event listener for messages
 client.on("message", msg => {
   if (msg.content === "ping") {
     msg.reply("Sprich Deutsch du Hurensohn!")
   }
 })
 
+// Send the client down a slide upon entry of specific channel
 client.on('voiceStateUpdate', (oldVoiceState, newVoiceState) => {
   let pc = client.channels.fetch(`712014015861096592`);
   console.log(newVoiceState.channelID);
@@ -18,4 +27,6 @@ client.on('voiceStateUpdate', (oldVoiceState, newVoiceState) => {
     console.log("Rutschen!")
   }
 })
+
+// Log in bot using token
 client.login("NzEyMDczNjc4NTU2NDk1OTQy.XsMQeA.m_uOt7yRep0fiAQ_QkHN1GuFBAI")
