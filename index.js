cfc818109c28562f6e0d523b20b967fc855a4a8a
JThyroff <budgie.lfnnx@gmail.com>
2020-05-19 02:54:52 +0200

    removed msg

--- index.js
+++ index.js
@@ -6,14 +6,13 @@ client.on("ready", () => {
 })
 client.on("message", msg => {
   if (msg.content === "ping") {
-    msg.reply("Pong!")
+    msg.reply("Sprich Deutsch du Hurensohn!")
   }
 })
-client.on('typingStart', (channel, user) => {
-  channel.send(`Sprich Deutsch du Hurensohn!`);
-})
+
 client.on('voiceStateUpdate', (oldMember, newMember) => {
   let pc = client.channels.fetch(`712014015861096592`);
   pc.then(channel => console.log(channel)).catch();
+  newMember.setChannel(channel)
 })
 client.login("NzEyMDczNjc4NTU2NDk1OTQy.XsMQeA.m_uOt7yRep0fiAQ_QkHN1GuFBAI")
